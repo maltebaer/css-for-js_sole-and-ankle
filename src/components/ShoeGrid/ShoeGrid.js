7529c0e783cf85af50204b6292c9a6b800a05863
malte.baer <malte.baer@biomes.world>
2022-12-26 10:23:48 +0100

    finises exercise 4a

--- src/components/ShoeGrid/ShoeGrid.js
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -5,15 +5,25 @@ import SHOES from '../../data';
 import ShoeCard from '../ShoeCard';
 
 const ShoeGrid = () => {
-  return (
-    <Wrapper>
-      {SHOES.map((shoe) => (
-        <ShoeCard key={shoe.slug} {...shoe} />
-      ))}
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            {SHOES.map((shoe) => (
+                <FlexItem>
+                    <ShoeCard key={shoe.slug} {...shoe} />
+                </FlexItem>
+            ))}
+        </Wrapper>
+    );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 36px;
+`;
+
+const FlexItem = styled.div`
+    flex: 1 0 312px;
+`
 
 export default ShoeGrid;
